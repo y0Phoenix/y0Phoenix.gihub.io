dc4fe37b85f7d86d041ca998670c870184a06036
Aaron <aarongraybill3@gmail.com>
2022-01-12 14:36:10 -0600

    init layout in app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Fragment, useEffect} from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Landing />}>
+            <Route element={<Container />}>
+              <Route exact path='/projects' element={<Projects />}/>
+              <Route exact path='/aboutme' element={<Bio />}/>
+              <Route exact path='contact' element={<Contact />}/>
+            </Route>
+          </Route>
+        </Routes>
+      </Fragment>
+    </Router>
   );
 }
 
