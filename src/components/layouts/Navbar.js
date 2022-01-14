edf57e2bcb38bb38054d8adc789b46595e37a3db
Aaron <aarongraybill3@gmail.com>
2022-01-13 18:43:15 -0600

    added nav link active change

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component, createRef, Fragment, useRef } from 'react'
+import React, { Component, createRef, Fragment, useEffect, useRef } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 const Navbar = () => {
     const homeLink = useRef();
@@ -6,9 +6,9 @@ const Navbar = () => {
     const aboutMeLink = useRef();
     const contactLink = useRef();
 
-    
     const location = useLocation();
-    function updateNavLinks () {
+
+    useEffect(() => {
         const navArr = [
             homeLink,
             portfolioLink,
@@ -16,35 +16,32 @@ const Navbar = () => {
             contactLink
         ];
         navArr.forEach(element => {
-            if (element.current.className === 'nav link active') {
-                console.log(`${element.current.className} for ${element.current.id}`);
-                element.current.className = 'nav link';
-                console.log(`${element.current.className} for ${element.current.id}`);
-
+            if (element.current.className === 'nav-link active') {
+                element.current.className = 'nav-link';
             }
             if(element.current.id === location.pathname) {
-                element.current.className = 'nav link active';
+                element.current.className = 'nav-link active';
             }
         });
-    }
+    }, [location])
     
     return (
         <Fragment>
-            <h1 className='header'><Link to='/'>Aaron Graybill</Link></h1>
+            <h1 className='header'><Link to='/' className='header-link'>Aaron Graybill</Link></h1>
             <div className='container'>
                 <header className='d-flex justify-content-center py-3'>
                     <ul className='nav nav-pills'>
                         <li className='nav-item'>
-                            <Link to='/' className='nav-link active' aria-current="page" ref={homeLink} id='/' onClick={updateNavLinks}>Home</Link>
+                            <Link to='/' className='nav-link active' aria-current="page" ref={homeLink} id='/'>Home</Link>
                         </li>
                         <li className='nav-item'>
-                            <Link to='/portfolio' className='nav-link' aria-current="page" ref={portfolioLink} id='/portfolio' onClick={updateNavLinks}>Portfolio</Link>
+                            <Link to='/portfolio' className='nav-link' aria-current="page" ref={portfolioLink} id='/portfolio'>Portfolio</Link>
                         </li>
                         <li className='nav-item'>
-                            <Link to='/aboutme' className='nav-link' aria-current="page" ref={aboutMeLink} id='/aboutme' onClick={updateNavLinks}>About Me</Link>
+                            <Link to='/aboutme' className='nav-link' aria-current="page" ref={aboutMeLink} id='/aboutme'>About Me</Link>
                         </li>
                         <li className='nav-item'>
-                            <Link to='/contact' className='nav-link' aria-current="page" ref={contactLink} id='/contact' onClick={updateNavLinks}>Contact</Link>
+                            <Link to='/contact' className='nav-link' aria-current="page" ref={contactLink} id='/contact'>Contact</Link>
                         </li>
                     </ul>
                 </header>
