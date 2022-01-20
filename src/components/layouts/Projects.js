6b8ea3bb9d855423f8e5851b63091653eb778a8c
Aaron <aarongraybill3@gmail.com>
2022-01-19 22:05:01 -0600

    eye-movement on portfolio page

--- src/components/layouts/Projects.js
+++ src/components/layouts/Projects.js
@@ -90,6 +90,26 @@ const Projects = () => {
                                     <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false">
                                         <title>Placeholder</title>
                                         <rect width='100%' height='100%' fill='#55595c'></rect>
+                                        <text x='50%' y='50%' fill='#eceeef' dy='-.5em' dx='-3.5em'>Eye Movement</text>
+                                    </svg>
+                                </div>
+                                <div className='card-body'>
+                                        <p className='card-text'>
+                                            Eye balls that follow the mouse around the screen using window global variables
+                                        </p>
+                                        <div className='d-flex justify-content-between align-items-center'>
+                                            <div className='btn-group'>
+                                                <a href='https://y0phoenix.github.io/eye-movement/' className='btn btn-sm btn-outline-secondary'>Site</a>
+                                                <a href='https://github.com/y0Phoenix/eye-movement' className='btn btn-sm btn-outline-secondary'>Repository</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                            </div>
+                            <div className='col'>
+                                <div className='card shadow-sm'>
+                                    <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false">
+                                        <title>Placeholder</title>
+                                        <rect width='100%' height='100%' fill='#55595c'></rect>
                                         <text x='50%' y='50%' fill='#eceeef' dy='-.5em' dx='-3.5em'>Bust Stop Viewer</text>
                                     </svg>
                                     <div className='card-body'>
