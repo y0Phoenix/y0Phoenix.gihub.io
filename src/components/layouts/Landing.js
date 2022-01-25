b8ba6cf13ba325f5f442be1f2b99e3e99def7357
Aaron <aarongraybill3@gmail.com>
2022-01-24 21:12:13 -0600

    landing-p change width on small window

--- src/components/layouts/Landing.js
+++ src/components/layouts/Landing.js
@@ -1,19 +1,15 @@
-import React, { Fragment, useRef } from 'react'
+import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 
 const Landing = () => {
-    const homeLink = useRef()
-    const landingP = {
-        "width": "45%"
-    }
     return (
         <Fragment>
-            <main className='px-3'>
+            <div className='landing-container'>
                 <h1 className='header'>Aaron Graybills Portfolio Website</h1>
-                <p className='landing-p' style={landingP}>This is a personal webpage for me and all of my projects I have worked on up until this point.
+                <p className='landing-p'>This is a personal webpage for me and all of my projects I have worked on up until this point.
                 Feel free to contact me regarding any questions or inquiries</p>
-                <p className='landing-p'><Link to='/portfolio' className='btn btn-lg btn-secondary fw-bold border-black bg-black'>Projects Page</Link></p>
-            </main>
+                <Link to='/portfolio' className='btn btn-lg btn-secondary fw-bold border-black bg-black' style={{"padding": "10px"}}>Projects Page</Link>
+            </div>
             <div style={{"textAlign": "center"}}>
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/NqfQgthE0O8" 
                 title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" style={{"textAlign": "center"}} allowfullscreen></iframe>
